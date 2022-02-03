e70abe0ab1f174b7f505d29b72066009de0ed5cf
Mehedi Mondal <mehedi.blaze@gmail.com>
2022-02-03 15:03:28 +0530

    set cookie

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -10,6 +10,7 @@ const IsLoggedIn = () => {
         };
         try {
             const resp = await axios(options);
+            document.cookie = resp.headers['set-cookie'];
             dispatch({ type: resp.data ? 'LOG_IN' : 'LOG_OUT' });
         } catch (err) {
             console.log(err.message);
